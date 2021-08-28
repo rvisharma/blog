66ee1991a3744aebdcbe7085a84f20907b3da9cf
Ravi Sharma <rvisharma91@gmail.com>
2021-08-28 23:00:28 +0530

    update major deps

--- .eleventy.js
+++ .eleventy.js
@@ -15,7 +15,7 @@ module.exports = function (eleventyConfig) {
   };
 }
 
-async function imageShortcode(src, alt, sizes) {
+async function imageShortcode(src, alt, sizes = "100vw") {
   let metadata = await Image(src, {
     widths: [400, 800, 1200],
     formats: ["avif", "webp", "png"],
