883f55f53284930bdd6d7dcac7c3d79823c80639
Ravi Sharma <rvisharma91@gmail.com>
2021-08-29 20:21:16 +0530

    update styles
    - use tailwind in jit mode
    - add custom fonts
    - adjust typography plugin to use custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 module.exports = {
+  mode: 'jit',
   purge: ["./dist/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            fontFamily: theme('fontFamily.serif'),
+            a: {
+              fontWeight: theme('fontWeight.semibold')
+            },
+            'h1,h2,h3,h4,h5,h6': {
+              fontFamily: theme('fontFamily.display'),
+            },
+            'h1,h2': {
+              fontWeight: theme('fontWeight.bold')
+            },
+            'h3,h4,h5,h6': {
+              fontWeight: theme('fontWeight.semibold')
+            }
+          },
+        },
+      }),
+      fontFamily: {
+        serif: '"Source Serif Pro", ui-serif, Georgia, Cambria, "Times New Roman", Times, serif;',
+        display: '"Philosopher", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
+      }
+    },
   },
   variants: {
     extend: {},
